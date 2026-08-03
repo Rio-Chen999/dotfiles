550a90d27629a0fb308812501a14a8a7381db46c
Rio-Chen999 <1627859138@qq.com>
2026-08-03 18:06:36 +0800

    neovim add plugin nvim-lspconfig

--- nvim/lua/package.lua
+++ nvim/lua/package.lua
@@ -1,2 +1,13 @@
-vim.pack.add({
+vim.pack.add({ 
+	{src = 'https://github.com/neovim/nvim-lspconfig'}
 })
+
+vim.lsp.config('rust-analyzer', {})
+vim.lsp.enable('rust-analyzer')
+vim.lsp.config('ts_ls', {})
+vim.lsp.enable('ts_ls')
+
+vim.keymap.set({'n'}, 'K', vim.lsp.buf.hover, {noremap = true, silent = true})
+vim.keymap.set({'n'}, 'gd', vim.lsp.buf.definition, {noremap = true, silent = true})
+vim.keymap.set({'n'}, 'gr', vim.lsp.buf.references, {noremap = true, silent = true})
+vim.keymap.set({'n'}, '<leader>ca', vim.lsp.buf.code_action, {noremap = true, silent = true})
